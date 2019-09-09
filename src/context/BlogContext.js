b9f69397443abbbc32a90831b01009ecc7f6e43e
Dilip <akshaykumar@Dilips-MacBook-Pro.local>
2019-09-09 17:29:06 +0530

    dynamic context data with callback

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
+    const [blogPosts, setBlogPosts] = useState([]);
 
-    return <BlogContext.Provider>
-        { children }
+    const addBlogPost = () => {
+        setBlogPosts([...blogPosts, { title: `Blog Post #${blogPosts.length + 1}` }])
+    };
+
+    return <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlogPost }}>
+        {children}
     </BlogContext.Provider>
 };
+
+export default BlogContext;
